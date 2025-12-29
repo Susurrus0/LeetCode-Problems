fdf8f359a5dcdc5f3b9397cd9545406b638b2088
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2025-12-29 18:00:43 +0100

    Write starting code

--- 977. Squares of a Sorted Array/main.py
+++ 977. Squares of a Sorted Array/main.py
@@ -1,7 +1,21 @@
+# LeetCode 977. Squares of a Sorted Array
+# https://leetcode.com/problems/squares-of-a-sorted-array
+
 def main():
-    # Your code goes here
+    # Test cases and expected output
+    nums1: list[int] = [-4,-1,0,3,10]
+    expected1: list[int] = [0,1,9,16,100]
+
+    nums2: list[int] = [-7,-3,2,3,11]
+    expected2: list[int] = [4,9,9,49,121]
     
+    Solution.sortedSquares(nums1)
+
     pass
+
+class Solution:
+    def sortedSquares(self, nums: list[int]) -> list[int]:
+        pass
 
 if __name__ == '__main__':
     main()
