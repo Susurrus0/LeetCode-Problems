1f9323fa5e1157a72a744cda44108c6da1b5299b
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2025-12-29 18:12:01 +0100

    Implement sortedSquares()

--- 977. Squares of a Sorted Array/main.py
+++ 977. Squares of a Sorted Array/main.py
@@ -9,13 +9,28 @@ def main():
     nums2: list[int] = [-7,-3,2,3,11]
     expected2: list[int] = [4,9,9,49,121]
     
-    Solution.sortedSquares(nums1)
+    print("** Test case 1 **")
+    print(f"nums1 = {nums1}")
+    print(f"Expected ouput = {expected1}")
+
+    print("** Test case 2 **")
+    print(f"nums2 = {nums2}")
+    print(f"Expected ouput = {expected2}")
+
+    solution = Solution()
+    print("** Results **")
+    print(f"result1 = {solution.sortedSquares(nums1)}")
+    print(f"result2 = {solution.sortedSquares(nums2)}")
 
     pass
 
 class Solution:
     def sortedSquares(self, nums: list[int]) -> list[int]:
-        pass
+        squaredNums: list[int] = []
+        for num in nums:
+            squaredNums.append(pow(num, 2))
+        squaredNums.sort()
+        return squaredNums
 
 if __name__ == '__main__':
     main()
