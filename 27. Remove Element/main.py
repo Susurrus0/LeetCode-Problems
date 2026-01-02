317c94693f764d80148232846e0916c5aee50ab5
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-02 20:41:19 +0100

    Start implementing removeElement()

--- 27. Remove Element/main.py
+++ 27. Remove Element/main.py
@@ -3,7 +3,10 @@
 
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
-        pass
+        for element in nums:
+            if element == val:
+                nums.pop(nums.index(element))
+        return len(nums)
 
 def main():
     # Test cases
@@ -14,6 +17,20 @@ def main():
     nums_2: list[int] = [0,1,2,2,3,0,4,2]
     val_2: int = 2
     expected_2: str = "5, nums = [0,1,4,0,3,_,_,_]"
+
+    solution = Solution()
+
+    print("Test Case #1")
+    print(f"nums_1 = {nums_1}, val_1 = {val_1}")
+    print(f"expected output = {expected_1}")
+    result_1: int = solution.removeElement(nums_1, val_1)
+    print(f"actual output = {result_1}, nums = {nums_1}")
+    print("")
+    print("Test Case #2")
+    print(f"nums_2 = {nums_2}, val_2 = {val_2}")
+    print(f"expected output = {expected_2}")
+    result_2: int = solution.removeElement(nums_2, val_2)
+    print(f"actual output = {result_2}, nums = {nums_2}")
 
 if __name__ == '__main__':
     main()
