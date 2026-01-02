26be424f4b41c59a3a5e12fb1b91a48a99ef52e9
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-02 20:16:31 +0100

    Write test cases

--- 27. Remove Element/main.py
+++ 27. Remove Element/main.py
@@ -6,9 +6,14 @@ class Solution:
         pass
 
 def main():
-    # Your code goes here
-    
-    pass
+    # Test cases
+    nums_1: list[int] = [3,2,2,3]
+    val_1: int = 3
+    expected_1: str = "2, nums = [2,2,_,_]"
+
+    nums_2: list[int] = [0,1,2,2,3,0,4,2]
+    val_2: int = 2
+    expected_2: str = "5, nums = [0,1,4,0,3,_,_,_]"
 
 if __name__ == '__main__':
     main()
