22d0b0e7d4257cf62e79e0b352c739cfea5337ce
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-02 20:14:05 +0100

    Add main.py

---
+++ 27. Remove Element/main.py
@@ -0,0 +1,14 @@
+# LeetCode 27. Remove Element
+# https://leetcode.com/problems/remove-element
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        pass
+
+def main():
+    # Your code goes here
+    
+    pass
+
+if __name__ == '__main__':
+    main()
