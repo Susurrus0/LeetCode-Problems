19c5b65bda65addf585511111fff21974e00ff5c
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-03 15:43:47 +0100

    Add main.py

---
+++ biweeklyContest173/main.py
@@ -0,0 +1,15 @@
+# LeetCode Biweekly Contest 173
+# TODO-1 Q1: Reverse String Prefix
+# Reverse the first k characters of s and return the resulting string. ©leetcode
+
+class Solution:
+    def reversePrefix(self, s: str, k: int) -> str:
+        pass
+
+def main():
+    # Your code goes here
+    
+    pass
+
+if __name__ == '__main__':
+    main()
