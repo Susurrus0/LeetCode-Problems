5315684b273194ca72158f1a1fd0bf3c09bb64f3
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-06 00:31:57 +0100

    Start implementing removeDuplicates()

--- 26. Remove Duplicates from Sorted Array/main.c
+++ 26. Remove Duplicates from Sorted Array/main.c
@@ -17,9 +17,26 @@ int main() {
     int numsSize_2 = sizeof(nums_2) / sizeof(nums_2[0]);
     char expected_2[] = "5, nums = [0,1,2,3,4,_,_,_,_,_]";
     
+    printf("Test Case #1\n");
+    printf("Expected output = %s\n", expected_1);
+    int result_1 = removeDuplicates(nums_1, numsSize_1);
+    printf("Actual output   = %d\n", result_1);
+    printf("\n");
+    printf("Test Case #2\n");
+    printf("Expected output = %s\n", expected_2);
+    int result_2 = removeDuplicates(nums_2, numsSize_2);
+    printf("Actual output   = %d\n", result_2);
+
     return 0;
 }
 
 int removeDuplicates(int* nums, int numsSize) {
+    int usedNums[numsSize];
+    int usedNumsIndex = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        usedNumsIndex = 0;
+    }
+
     return 0;
 }
