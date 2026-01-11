3ecc79d4bcd9f6a9b3ef4c8547538ced5cd90a20
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-11 20:21:01 +0100

    Implement removeDuplicates()

--- 26. Remove Duplicates from Sorted Array/main.py
+++ 26. Remove Duplicates from Sorted Array/main.py
@@ -3,7 +3,14 @@
 
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        pass
+        if not nums:
+            return 0
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
 
 def main():
     nums_1: list[int] = [1,1,2]
@@ -18,13 +25,13 @@ def main():
     print("Expected output:")
     print(f"    {expected_1}")
     print("Actual output:")
-    print(f"    ...")
+    print(f"    {solution.removeDuplicates(nums_1)}, nums = {nums_1}")
     print("")
     print("Test Case #2")
     print("Expected output:")
     print(f"    {expected_2}")
     print("Actual output:")
-    print(f"    ...")
+    print(f"    {solution.removeDuplicates(nums_2)}, nums = {nums_2}")
 
 if __name__ == '__main__':
     main()
