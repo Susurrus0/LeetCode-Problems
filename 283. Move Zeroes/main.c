40e9f41bd4d664c9909bf3b9a257527529736ccf
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-11 20:36:01 +0100

    Implement printArray()

--- 283. Move Zeroes/main.c
+++ 283. Move Zeroes/main.c
@@ -6,6 +6,7 @@ https://leetcode.com/problems/move-zeroes
 #include <stdio.h>
 
 void moveZeroes(int* nums, int numsSize);
+void printArray(int* array, int size);
 
 int main() {
     int nums_1[] = { 0,1,0,3,12 };
@@ -19,16 +20,31 @@ int main() {
     printf("Test Case #1\n");
     printf("Expected output:\n");
     printf("    %s\n", expected_1);
+    printf("Actual output:\n");
+    printf("    ...\n");
 
     printf("\n");
 
     printf("Test Case #2\n");
     printf("Expected output:\n");
     printf("    %s\n", expected_2);
-    
+    printf("Expected output:\n");
+    printf("    ...\n");
+
     return 0;
 }
 
 void moveZeroes(int* nums, int numsSize) {
     return;
+}
+
+void printArray(int* array, int size) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d", array[i]);
+        if (i + 1 < size) {
+            printf(",");
+        }
+    }
+    printf("]");
 }
