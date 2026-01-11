e6139c51275d4d81a2d4426c2f6afb7cc9893e3b
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-11 19:28:51 +0100

    Remove main.c, add main.py

---
+++ 26. Remove Duplicates from Sorted Array/main.py
@@ -0,0 +1,30 @@
+# LeetCode 26. Remove Duplicates from Sorted Array
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array
+
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        pass
+
+def main():
+    nums_1: list[int] = [1,1,2]
+    nums_2: list[int] = [0,0,1,1,1,2,2,3,3,4]
+
+    expected_1: str = "2, nums = [1,2,_]"
+    expected_2: str = "5, nums = [0,1,2,3,4,_,_,_,_,_]"
+    
+    solution: Solution = Solution()
+
+    print("Test Case #1")
+    print("Expected output:")
+    print(f"    {expected_1}")
+    print("Actual output:")
+    print(f"    ...")
+    print("")
+    print("Test Case #2")
+    print("Expected output:")
+    print(f"    {expected_2}")
+    print("Actual output:")
+    print(f"    ...")
+
+if __name__ == '__main__':
+    main()
