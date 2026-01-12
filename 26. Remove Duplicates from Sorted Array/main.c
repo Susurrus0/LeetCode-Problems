504616babff151ba38b27733920a58df344a27ec
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-12 22:13:35 +0100

    Start implementing removeDuplicates() using the two-pointer approach

--- 26. Remove Duplicates from Sorted Array/main.c
+++ 26. Remove Duplicates from Sorted Array/main.c
@@ -1,7 +1,68 @@
+/*****************************************************
+LeetCode 26. Remove Duplicates from Sorted Array
+https://leetcode.com/problems/remove-duplicates-from-sorted-array
+*****************************************************/
+
 #include <stdio.h>
 
+int removeDuplicates(int* nums, int numsSize);
+void printArray(int* array, int size);
+
 int main() {
-    // Your code goes here
-    
+    int nums_1[] = { 1,1,2 };
+    int numsSize_1 = sizeof(nums_1) / sizeof(nums_1[0]);
+    int nums_2[] = { 0,0,1,1,1,2,2,3,3,4 };
+    int numsSize_2 = sizeof(nums_2) / sizeof(nums_2[0]);
+
+    char expected_1[] = "2, nums = [1,2,_]";
+    char expected_2[] = "5, nums = [0,1,2,3,4,_,_,_,_,_]";
+
+    printf("Test Case #1\n");
+    printf("Expected output:\n");
+    printf("    %s\n", expected_1);
+    printf("Actual output:\n");
+    printf("    %d, nums = ", removeDuplicates(nums_1, numsSize_1));
+    printArray(nums_1, numsSize_1);
+
+    printf("\n");
+
+    printf("Test Case #2\n");
+    printf("Expected output:\n");
+    printf("    %s\n", expected_2);
+    printf("Actual output:\n");
+    printf("    %d, nums = ", removeDuplicates(nums_2, numsSize_2));
+    printArray(nums_2, numsSize_2);
+    printf("\n");
+
     return 0;
+}
+
+void printArray(int* array, int size) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d", array[i]);
+        if (i + 1 < size) {
+            printf(",");
+        }
+    }
+    printf("]");
+}
+
+int removeDuplicates(int* nums, int numsSize) {
+    int uniques = 0;
+    int lastUnique = nums[0];
+    int i = 0;
+    int j = 0; // index of last unique num
+    for (; i < numsSize; i++) {
+        if (nums[i] == lastUnique) {
+            nums[j] = nums[i];
+            j++;
+            uniques++;
+            lastUnique = nums[i];
+        }
+    }
+    for (; j < numsSize; j++) {
+        nums[j] = 0;
+    }
+    return uniques;
 }
