4431b150476913ec23065cb89672665c5224426c
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-13 00:07:23 +0100

    Add main.c

---
+++ 1346. Check If N and Its Double Exist/main.c
@@ -0,0 +1,12 @@
+/*****************************************************
+LeetCode 1346. Check If N and Its Double Exist
+https://leetcode.com/problems/check-if-n-and-its-double-exist
+*****************************************************/
+
+#include <stdio.h>
+
+int main() {
+    // Your code goes here
+    
+    return 0;
+}
