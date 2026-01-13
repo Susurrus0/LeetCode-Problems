5bee9886fb72e99a627c265ae700afef5a041744
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-13 01:16:27 +0100

    Add main.c

---
+++ 941. Valid Mountain Array/main.c
@@ -0,0 +1,51 @@
+/*****************************************************
+LeetCode 941. Valid Mountain Array
+https://leetcode.com/problems/valid-mountain-array
+*****************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool validMountainArray(int* arr, int arrSize);
+
+int main() {
+    int arr_1[] = { 2,1 };
+    int arrLen_1 = sizeof(arr_1) / sizeof(arr_1[0]);
+    bool expected_1 = false;
+
+    int arr_2[] = { 3,5,5 };
+    int arrLen_2 = sizeof(arr_2) / sizeof(arr_2[0]);
+    bool expected_2 = false;
+
+    int arr_3[] = { 0,3,2,1 };
+    int arrLen_3 = sizeof(arr_3) / sizeof(arr_3[0]);
+    bool expected_3 = true;
+
+    printf("Test Case #1\n");
+    printf("Expected output:\n");
+    printf("    %s\n", expected_1 ? "true" : "false");
+    printf("Actual output:\n");
+    printf("    %s\n", validMountainArray(arr_1, arrLen_1) ? "true" : "false");
+
+    printf("\n");
+
+    printf("Test Case #2\n");
+    printf("Expected output:\n");
+    printf("    %s\n", expected_2 ? "true" : "false");
+    printf("Actual output:\n");
+    printf("    %s\n", validMountainArray(arr_2, arrLen_2) ? "true" : "false");
+
+    printf("\n");
+
+    printf("Test Case #3\n");
+    printf("Expected output:\n");
+    printf("    %s\n", expected_3 ? "true" : "false");
+    printf("Actual output:\n");
+    printf("    %s\n", validMountainArray(arr_3, arrLen_3) ? "true" : "false");
+    
+    return 0;
+}
+
+bool validMountainArray(int* arr, int arrSize) {
+    return false;
+}
