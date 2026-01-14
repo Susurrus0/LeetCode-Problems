46cb2a1f441b86b94ad83fbf65270eb5ed63a105
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-14 16:03:33 +0100

    Add main.py, remove main.c

--- 1299. Replace Elements with Greatest Element on Right Side/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Your code goes here
-    
-    return 0;
-}
---
+++ 1299. Replace Elements with Greatest Element on Right Side/main.py
@@ -0,0 +1,16 @@
+# LeetCode 1299. Replace Elements with Greatest Element on Right Side
+# https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side
+
+class Solution:
+    def replaceElements(self, arr: list[int]) -> list[int]:
+        pass
+
+
+def main():
+    # Your code goes here
+    
+    pass
+
+
+if __name__ == '__main__':
+    main()
