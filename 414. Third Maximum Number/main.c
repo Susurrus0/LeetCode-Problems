47be2bb7c197bd60d38d64eb4c45b6c77d10c405
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-16 00:33:52 +0100

    Start implementing thirdMax() and add Test Case #4

--- 414. Third Maximum Number/main.c
+++ 414. Third Maximum Number/main.c
@@ -21,6 +21,10 @@ int main() {
     int numsLen_3 = sizeof(nums_3) / sizeof(nums_3[0]);
     int expected_3 = 1;
 
+    int nums_4[] = { 1,1,2 };
+    int numsLen_4 = sizeof(nums_4) / sizeof(nums_4[0]);
+    int expected_4 = 2;
+
     printf("--- Test Case #1 ---\n");
     printf("Expected output:\n");
     printf("\t%i\n", expected_1);
@@ -43,9 +47,38 @@ int main() {
     printf("Actual output:\n");
     printf("\t%i\n", thirdMax(nums_3, numsLen_3));
 
+    printf("\n");
+
+    printf("--- Test Case #4 ---\n");
+    printf("Expected output:\n");
+    printf("\t%i\n", expected_4);
+    printf("Actual output:\n");
+    printf("\t%i\n", thirdMax(nums_4, numsLen_4));
+
     return 0;
 }
 
 int thirdMax(int* nums, int numsSize) {
-    return 0;
+    int max1 = nums[0];
+    int max2 = 0, max3 = 0;
+
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] > max1)
+            max1 = nums[i];
+    }
+
+    if (numsSize < 3)
+        return max1;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != max1 && nums[i] > max2)
+            max2 = nums[i];
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != max1 && nums[i] != max2 && nums[i] > max3)
+            max3 = nums[i];
+    }
+
+    return max3;
 }
