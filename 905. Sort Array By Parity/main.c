bcab4a41adb76ddb5d782de73263a3075d5b7881
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-15 18:28:34 +0100

    Add main.c

---
+++ 905. Sort Array By Parity/main.c
@@ -0,0 +1,19 @@
+/*****************************************************
+LeetCode 905. Sort Array By Parity
+https://leetcode.com/problems/sort-array-by-parity
+*****************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* sortArrayByParity(int* nums, int numsSize, int* returnSize);
+
+int main() {
+    // Your code goes here
+    
+    return 0;
+}
+
+int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
+    
+}
