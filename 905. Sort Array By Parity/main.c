8009dc956fb7e322dec35d9024c3d4b651a31905
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-15 19:25:02 +0100

    Implement sortArrayByParity()

--- 905. Sort Array By Parity/main.c
+++ 905. Sort Array By Parity/main.c
@@ -6,8 +6,8 @@ https://leetcode.com/problems/sort-array-by-parity
 #include <stdio.h>
 #include <stdlib.h>
 
-int* sortArrayByParity(int* nums, int numsSize, int* returnSize);
-void printArray(int* array, int size);
+int *sortArrayByParity(int *nums, int numsSize, int *returnSize);
+void printArray(int *array, int size);
 
 int main() {
     int nums_1[] = { 3,1,2,4 };
@@ -16,14 +16,14 @@ int main() {
 
     int nums_2[] = { 0 };
     int numsLen_2 = sizeof(nums_2) / sizeof(nums_2[0]);
-    char expected_2 = "[0]";
+    char expected_2[] = "[0]";
 
     printf("Test Case #1\n");
     printf("Expected output:\n");
     printf("\t%s\n", expected_1);
     printf("Actual output:\n");
     printf("\t");
-    printArray(sortArrayByParity(nums_1, numsLen_1, numsLen_1), numsLen_1);
+    printArray(sortArrayByParity(nums_1, numsLen_1, &numsLen_1), numsLen_1);
 
     printf("\n\n");
 
@@ -32,9 +32,9 @@ int main() {
     printf("\t%s\n", expected_2);
     printf("Actual output:\n");
     printf("\t");
-    printArray(sortArrayByParity(nums_2, numsLen_2, numsLen_2), numsLen_2);
+    printArray(sortArrayByParity(nums_2, numsLen_2, &numsLen_2), numsLen_2);
 
-    print("\n");
+    printf("\n");
 
     return 0;
 }
@@ -42,8 +42,24 @@ int main() {
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
-    int* result = (int*) malloc(numsSize * sizeof(int));
+int *sortArrayByParity(int *nums, int numsSize, int *returnSize) {
+    int *result = (int*) malloc(numsSize * sizeof(int));
+
+    int evenIndex = 0;
+    int oddIndex = numsSize - 1;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] % 2 == 0) {
+            result[evenIndex] = nums[i];
+            evenIndex++;
+        }
+        else {
+            result[oddIndex] = nums[i];
+            oddIndex--;
+        }
+    }
+    *returnSize = numsSize;
+    return result;
 }
 
 void printArray(int* array, int size) {
