cb2de6fcbba9306a2ee46a9af17dbf37ee1ecd99
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-16 22:55:49 +0100

    Add main.py

---
+++ 448. Find All Numbers Disappeared in an Array/main.py
@@ -0,0 +1,33 @@
+# LeetCode 448. Find All Numbers Disappeared in an Array
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+
+class Solution:
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        pass
+
+
+def main():
+    # Test cases
+    nums_1: list[int] = [4,3,2,7,8,2,3,1]
+    expected_1: list[int] = [5,6]
+
+    nums_2: list[int] = [1,1]
+    expected_2: list[int] = [2]
+
+    solution: Solution = Solution()
+
+    print("--- Test case 1 ---")
+    print(f"nums_1 = {nums_1}")
+    print(f"Expected ouput:\n\t{expected_1}")
+    print(f"Actual output:\n\t{solution.findDisappearedNumbers(nums_1)}")
+
+    print()
+
+    print("--- Test case 2 ---")
+    print(f"nums_2 = {nums_2}")
+    print(f"Expected ouput:\n\t{expected_2}")
+    print(f"Actual output:\n\t{solution.findDisappearedNumbers(nums_2)}")
+
+
+if __name__ == '__main__':
+    main()
