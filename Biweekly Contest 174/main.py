e7b17aa9602262ec8beebb651ed63d1e52750fcc
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-17 15:46:52 +0100

    Add main.py

---
+++ Biweekly Contest 174/main.py
@@ -0,0 +1,15 @@
+# LeetCode Biweekly Contest 173
+# TODO-1 Q1: Best Reachable Tower
+
+class Solution:
+    def bestTower(self, towers: list[list[int]], center: list[int], radius: int) -> list[int]:
+        pass
+
+def main():
+    # Your code goes here
+    
+    pass
+
+
+if __name__ == '__main__':
+    main()
