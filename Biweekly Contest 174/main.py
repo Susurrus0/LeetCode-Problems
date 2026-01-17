7b978ebae6cac279dcfd9e4ce98dc0cf26f7cd72
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-17 16:54:17 +0100

    Write Q2 test cases

--- Biweekly Contest 174/main.py
+++ Biweekly Contest 174/main.py
@@ -1,5 +1,7 @@
 # LeetCode Biweekly Contest 173
-# TODO-1 Q1: Best Reachable Tower
+# TODO-1 Q1. Best Reachable Tower
+
+# TODO-2 Q2. Minimum Operations to Reach Target Array
 
 class Solution:
     def bestTower(self, towers: list[list[int]], center: list[int], radius: int) -> list[int]:
@@ -28,48 +30,89 @@ class Solution:
                 bestTower = goodTower
 
         return bestTower[:2]
+    
+
+    def minOperations(self, nums: list[int], target: list[int]) -> int:
+        pass
 
 
 def main():
     # Q1 Test Cases
-    towers_1: list[list[int]] = [[1,2,5], [2,1,7], [3,1,9]]
-    center_1: list[int] = [1,1]
-    radius_1: int = 2
-    expected_1: list[int] = [3,1]
+    towers_1a: list[list[int]] = [[1,2,5], [2,1,7], [3,1,9]]
+    center_1a: list[int] = [1,1]
+    radius_1a: int = 2
+    expected_1a: list[int] = [3,1]
 
-    towers_2: list[list[int]] = [[1,3,4], [2,2,4], [4,4,7]]
-    center_2: list[int] = [0,0]
-    radius_2: int = 5
-    expected_2: list[int] = [1,3]
+    towers_2a: list[list[int]] = [[1,3,4], [2,2,4], [4,4,7]]
+    center_2a: list[int] = [0,0]
+    radius_2a: int = 5
+    expected_2a: list[int] = [1,3]
 
-    towers_3: list[list[int]] = [[5,6,8], [0,3,5]]
-    center_3: list[int] = [1,2]
-    radius_3: int = 1
-    expected_3: list[int] = [-1,-1]
+    towers_3a: list[list[int]] = [[5,6,8], [0,3,5]]
+    center_3a: list[int] = [1,2]
+    radius_3a: int = 1
+    expected_3a: list[int] = [-1,-1]
 
     solution: Solution = Solution()
 
+    print("********** Q1 **********")
+
     print("--- Test case 1 ---")
-    print(f"towers_1 = {towers_1}")
-    print(f"center_1 = {center_1}")
-    print(f"Expected ouput:\n\t{expected_1}")
-    print(f"Actual output:\n\t{solution.bestTower(towers_1, center_1, radius_1)}")
+    print(f"towers_1a = {towers_1a}")
+    print(f"center_1a = {center_1a}")
+    print(f"Expected ouput:\n\t{expected_1a}")
+    print(f"Actual output:\n\t{solution.bestTower(towers_1a, center_1a, radius_1a)}")
 
     print()
 
     print("--- Test case 2 ---")
-    print(f"towers_2 = {towers_2}")
-    print(f"center_2 = {center_2}")
-    print(f"Expected ouput:\n\t{expected_2}")
-    print(f"Actual output:\n\t{solution.bestTower(towers_2, center_2, radius_2)}")
+    print(f"towers_2a = {towers_2a}")
+    print(f"center_2a = {center_2a}")
+    print(f"Expected ouput:\n\t{expected_2a}")
+    print(f"Actual output:\n\t{solution.bestTower(towers_2a, center_2a, radius_2a)}")
 
     print()
 
     print("--- Test case 3 ---")
-    print(f"towers_3 = {towers_3}")
-    print(f"center_3 = {center_3}")
-    print(f"Expected ouput:\n\t{expected_3}")
-    print(f"Actual output:\n\t{solution.bestTower(towers_3, center_3, radius_3)}")
+    print(f"towers_3a = {towers_3a}")
+    print(f"center_3a = {center_3a}")
+    print(f"Expected ouput:\n\t{expected_3a}")
+    print(f"Actual output:\n\t{solution.bestTower(towers_3a, center_3a, radius_3a)}")
+
+    # Q2 Test Cases
+    nums_1b: list[int] = [1,2,3]
+    target_1b: list[int] = [2,1,3]
+    expected_1b: int = 2
+
+    nums_2b: list[int] = [4,1,4]
+    target_2b: list[int] = [5,1,4]
+    expected_2b: int = 1
+
+    nums_3b: list[int] = [7,3,7]
+    target_3b: list[int] = [5,5,9]
+    expected_3b: int = 2
+
+    print("********** Q2 **********")
+
+    print("--- Test case 1 ---")
+    print(f"nums_1b = {nums_1b}")
+    print(f"Expected ouput:\n\t{expected_1b}")
+    print(f"Actual output:\n\t{solution.minOperations(nums_1b, target_1b)}")
+
+    print()
+
+    print("--- Test case 2 ---")
+    print(f"nums_2b = {nums_2b}")
+    print(f"Expected ouput:\n\t{expected_2b}")
+    print(f"Actual output:\n\t{solution.minOperations(nums_2b, target_2b)}")
+
+    print()
+
+    print("--- Test case 3 ---")
+    print(f"nums_3b = {nums_3b}")
+    print(f"Expected ouput:\n\t{expected_3b}")
+    print(f"Actual output:\n\t{solution.minOperations(nums_3b, target_3b)}")
+
 
 
 if __name__ == '__main__':
