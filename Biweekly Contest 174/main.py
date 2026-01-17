c31df0b81f6700310f304ca23f1a8dde418e9bab
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-17 15:51:52 +0100

    Write test cases

--- Biweekly Contest 174/main.py
+++ Biweekly Contest 174/main.py
@@ -6,9 +6,45 @@ class Solution:
         pass
 
 def main():
-    # Your code goes here
-    
-    pass
+    # Q1 Test Cases
+    towers_1: list[list[int]] = [[1,2,5], [2,1,7], [3,1,9]]
+    center_1: list[int] = [1,1]
+    radius_1: int = 2
+    expected_1: list[int] = [3,1]
+
+    towers_2: list[list[int]] = [[1,3,4], [2,2,4], [4,4,7]]
+    center_2: list[int] = [0,0]
+    radius_2: int = 5
+    expected_2: list[int] = [1,3]
+
+    towers_3: list[list[int]] = [[5,6,8], [0,3,5]]
+    center_3: list[int] = [1,2]
+    radius_3: int = 1
+    expected_3: list[int] = [-1,-1]
+
+    solution: Solution = Solution()
+
+    print("--- Test case 1 ---")
+    print(f"towers_1 = {towers_1}")
+    print(f"center_1 = {center_1}")
+    print(f"Expected ouput:\n\t{expected_1}")
+    print(f"Actual output:\n\t{solution.bestTower(towers_1, center_1, radius_1)}")
+
+    print()
+
+    print("--- Test case 2 ---")
+    print(f"towers_2 = {towers_2}")
+    print(f"center_2 = {center_2}")
+    print(f"Expected ouput:\n\t{expected_2}")
+    print(f"Actual output:\n\t{solution.bestTower(towers_2, center_2, radius_2)}")
+
+    print()
+
+    print("--- Test case 3 ---")
+    print(f"towers_3 = {towers_3}")
+    print(f"center_3 = {center_3}")
+    print(f"Expected ouput:\n\t{expected_3}")
+    print(f"Actual output:\n\t{solution.bestTower(towers_3, center_3, radius_3)}")
 
 
 if __name__ == '__main__':
