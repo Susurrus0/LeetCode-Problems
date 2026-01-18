90fa00b3bfca0e8a53d9ee63ba8d35af9443320b
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-18 23:23:48 +0100

    Optimize sortedSquares() runtime

--- 977. Squares of a Sorted Array/main.py
+++ 977. Squares of a Sorted Array/main.py
@@ -3,34 +3,36 @@
 
 def main():
     # Test cases and expected output
-    nums1: list[int] = [-4,-1,0,3,10]
+    nums_1: list[int] = [-4,-1,0,3,10]
     expected1: list[int] = [0,1,9,16,100]
 
-    nums2: list[int] = [-7,-3,2,3,11]
+    nums_2: list[int] = [-7,-3,2,3,11]
     expected2: list[int] = [4,9,9,49,121]
     
-    print("** Test case 1 **")
-    print(f"nums1 = {nums1}")
-    print(f"Expected ouput = {expected1}")
+    solution: Solution = Solution()
 
-    print("** Test case 2 **")
-    print(f"nums2 = {nums2}")
-    print(f"Expected ouput = {expected2}")
+    print("--- Test case 1 ---")
+    print(f"nums_1 = {nums_1}")
+    print(f"Expected ouput:\n\t{expected1}")
+    print(f"Actual output:\n\t{solution.sortedSquares(nums_1)}")
+    print()
+    print("--- Test case 2 ---")
+    print(f"nums_2 = {nums_2}")
+    print(f"Expected ouput:\n\t{expected2}")
+    print(f"Actual output:\n\t{solution.sortedSquares(nums_2)}")
 
-    solution = Solution()
-    print("** Results **")
-    print(f"result1 = {solution.sortedSquares(nums1)}")
-    print(f"result2 = {solution.sortedSquares(nums2)}")
-
-    pass
 
 class Solution:
     def sortedSquares(self, nums: list[int]) -> list[int]:
-        squaredNums: list[int] = []
-        for num in nums:
-            squaredNums.append(pow(num, 2))
-        squaredNums.sort()
-        return squaredNums
+        # Initial solution
+        # squaredNums: list[int] = []
+        # for num in nums:
+        #     squaredNums.append(pow(num, 2))
+        # squaredNums.sort()
+        # return squaredNums
+        # Improved solution
+        return sorted([i*i for i in nums])
+
 
 if __name__ == '__main__':
     main()
