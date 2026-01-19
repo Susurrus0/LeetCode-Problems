375c223cf318c770cfddc4f84c95b807491c1c56
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-19 19:39:07 +0100

    Start implementing MyLinkedList

--- 707. Design Linked List/main.py
+++ 707. Design Linked List/main.py
@@ -2,24 +2,58 @@
 # https://leetcode.com/problems/design-linked-list
 
 class MyLinkedList:
-
     def __init__(self):
-        pass
+        self.head = self
+        self.nodes: list[MyLinkedList] = [self]
+        self.val: int = None
+        self.next: MyLinkedList = None
+
 
     def get(self, index: int) -> int:
-        pass
+        node: MyLinkedList = self.head
+        for i in range(index+1):
+            if node is None:
+                return -1
+            node = node.next
+        return node.val
+
 
     def addAtHead(self, val: int) -> None:
-        pass
+        newNode: MyLinkedList = MyLinkedList()
+        newNode.val = val
+        newNode.next = self.head
+        self.head = newNode
+
 
     def addAtTail(self, val: int) -> None:
-        pass
+        newNode: MyLinkedList = MyLinkedList()
+        newNode.val = val
+        node: MyLinkedList = self.head
+        while node.next is not None:
+            node = node.next
+        node.next = newNode
+
 
     def addAtIndex(self, index: int, val: int) -> None:
-        pass
+        node: MyLinkedList = self.head
+        newNode: MyLinkedList = MyLinkedList()
+        newNode.val = val
+
+        for i in range(index):
+            node = node.next
+        
+        node.next = newNode
+        newNode.next = node.next.next
+
 
     def deleteAtIndex(self, index: int) -> None:
-        pass
+        node: MyLinkedList = self.head
+        for i in range(index):
+            if node is None:
+                return
+            node = node.next
+        
+        node.next = node.next.next
 
 
 # Your MyLinkedList object will be instantiated and called as such:
