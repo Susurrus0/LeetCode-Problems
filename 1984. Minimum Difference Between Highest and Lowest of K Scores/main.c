d92a0980bc995b9bdf780eccfc8b5016dc89b9ba
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-25 16:05:43 +0100

    Write Test Case #3

--- 1984. Minimum Difference Between Highest and Lowest of K Scores/main.c
+++ 1984. Minimum Difference Between Highest and Lowest of K Scores/main.c
@@ -19,6 +19,11 @@ int main() {
     int numsLen_2 = sizeof(nums_2) / sizeof(nums_2[0]);
     int expected_2 = 2;
 
+    int nums_3[] = {41900,69441,94407,37498,20299,10856,36221,2231,54526,79072,84309,76765,92282,13401,44698,17586,98455,47895,98889,65298,32271,23801,83153,12186,7453,79460,67209,54576,87785,47738,40750,31265,77990,93502,50364,75098,11712,80013,24193,35209,56300,85735,3590,24858,6780,50086,87549,7413,90444,12284,44970,39274,81201,43353,75808,14508,17389,10313,90055,43102,18659,20802,70315,48843,12273,78876,36638,17051,20478};
+    int k_3 = 5;
+    int numsLen_3 = sizeof(nums_3) / sizeof(nums_3[0]);
+    int expected_3 = 1428;
+
     printf("--- Test Case #1 ---\n");
     printf("Expected output:\n");
     printf("\t%i\n", expected_1);
@@ -32,6 +37,14 @@ int main() {
     printf("\t%i\n", expected_2);
     printf("Actual output:\n");
     printf("\t%i\n", minimumDifference(nums_2, numsLen_2, k_2));
+
+    printf("\n");
+
+    printf("--- Test Case #3 ---\n");
+    printf("Expected output:\n");
+    printf("\t%i\n", expected_3);
+    printf("Actual output:\n");
+    printf("\t%i\n", minimumDifference(nums_3, numsLen_3, k_3));
 
     return 0;
 }
